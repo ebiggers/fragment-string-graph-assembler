0cd8b96cf5d7eb29469a5125980d972e45bd10c9
Eric Biggers <ebiggers3@gmail.com>
2012-12-12 02:47:23 -0600

    tee assemble.log

--- test/run-example.sh
+++ test/run-example.sh
@@ -4,4 +4,5 @@ make GENOME=E_coli.fa   \
      SAMPLE_SIZE=500000 \
      COVERAGE=5         \
      READ_LEN=100       \
-     MIN_OVERLAP_LEN=25
+     MIN_OVERLAP_LEN=25 \
+     2>&1 | tee assemble.log
